74d02c015cfccd5272ec3fb5397ac9a661a7b431
Anderson Pereira <anderson.rasec@outlook.com>
2016-12-17 00:02:23 -0300

    Class 1 Create the Class Product.

---
+++ DevStore.Domain/DevStore.Domain/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevStore.Domain
+{
+    class Product
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime AcquireDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
