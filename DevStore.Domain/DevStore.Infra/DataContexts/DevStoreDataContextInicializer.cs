a202bcb31ff1c4a0471d492f95634ade08bc9a3f
Anderson Pereira <anderson.rasec@outlook.com>
2016-12-19 16:32:57 -0300

    Class 3 Incluinded Products and Categories for the class DevStoreDataContextInicializer

--- DevStore.Domain/DevStore.Infra/DataContexts/DevStoreDataContextInicializer.cs
+++ DevStore.Domain/DevStore.Infra/DataContexts/DevStoreDataContextInicializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevStore.Domain;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -9,5 +10,24 @@ namespace DevStore.Infra.DataContexts
 {
     class DevStoreDataContextInicializer : DropCreateDatabaseIfModelChanges<DevStoreDataContext>
     {
+        protected override void Seed(DevStoreDataContext context)
+        {
+
+            context.Categories.Add(new Category { Id = 1, Title = "Informática"});
+
+            context.Categories.Add(new Category { Id = 2, Title = "Games"});
+
+            context.Categories.Add(new Category { Id = 3, Title = "Didático"});
+
+            context.SaveChanges();
+
+            context.Products.Add(new Product { Id = 1, Title = "Introdução á Lógica de Programação.", Price = 122, AcquireDate = new DateTime(2016, 5, 28), IsActive = true, CategoryId = 1});
+
+            context.Products.Add(new Product { Id = 2, Title = "Melhores games de 2016", Price = 82, AcquireDate = new DateTime(2016, 12, 15), IsActive = true, CategoryId = 2 });
+
+            context.Products.Add(new Product { Id = 3, Title = "História da Arte.", Price = 150, AcquireDate = new DateTime(2016, 3, 15), IsActive = true, CategoryId = 3 });
+
+            base.Seed(context);
+        }
     }
 }
