6a43ca214d7e80b177df202a73eade6287b8b624
Anderson Pereira <anderson.rasec@outlook.com>
2016-12-20 12:58:07 -0300

    Class 5 Clean the Project DevStore.Api for the use.

--- DevStore.Domain/DevStore.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace DevStore.Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- DevStore.Domain/DevStore.Api/Global.asax.cs
+++ DevStore.Domain/DevStore.Api/Global.asax.cs
@@ -16,8 +16,6 @@ namespace DevStore.Api
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
