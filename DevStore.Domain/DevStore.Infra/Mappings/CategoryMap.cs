d0a24fc87a057434ef7438f922d0dce0356944ce
Anderson Pereira <anderson.rasec@outlook.com>
2016-12-20 11:13:27 -0300

    Class 4 Mapped the class Category int he class CategoryMap.

--- DevStore.Domain/DevStore.Infra/Mappings/CategoryMap.cs
+++ DevStore.Domain/DevStore.Infra/Mappings/CategoryMap.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using DevStore.Domain;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DevStore.Infra.Mappings
 {
-    class CategoryMap
+    public class CategoryMap : EntityTypeConfiguration<Category>
     {
+        public CategoryMap()
+        {
+            //Inforam qual será o nome da tabela.
+            ToTable("Category");
+
+            //Informa qual será a chave primária.
+            HasKey(x => x.Id);
+
+            //Informa as características para a propriedade Title, tem no máximo 60 caracteres e é obrigatória.
+            //Acima de 60 caracteres dará erro.
+            Property(x => x.Title).HasMaxLength(60).IsRequired();
+
+        }
     }
 }
