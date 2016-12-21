264c78e8041d93ee6eed1c960e4b77648ab91aa5
Anderson Pereira <anderson.rasec@outlook.com>
2016-12-20 22:11:50 -0300

    Class 6 Modificaded the WebApiConfig for use the setings for Json.

--- DevStore.Domain/DevStore.Api/App_Start/WebApiConfig.cs
+++ DevStore.Domain/DevStore.Api/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,7 +12,24 @@ namespace DevStore.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            
+            //Configuração para o que o Json seja o formato principal da API.
+            
+            var formatters = GlobalConfiguration.Configuration.Formatters;
 
+            var jsonFormatter = formatters.JsonFormatter;
+
+            var settings = jsonFormatter.SerializerSettings;
+
+            //Configuráção para preservar a referência da serialização.
+            jsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.None;
+            //Remoção do XML.
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //Configuração do formato dde saída dos dados.
+            settings.Formatting = Formatting.Indented;
+            //Converter os nomes da propriedades em mínúsculas.
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
 
